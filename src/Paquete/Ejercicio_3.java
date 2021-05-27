cbe682687106ca7f9fadcf5d8de0582f91bc189f
Luisa Gil <luisamariagilgarcia24@gmail.com>
2021-05-27 14:01:26 -0500

    Se realiza ejercicio 3

---
+++ src/Paquete/Ejercicio_3.java
@@ -0,0 +1,24 @@
+package Paquete;
+
+import java.util.Scanner;
+
+public class Ejercicio_3 {
+
+    private static final double PI =3.1416 ;
+
+    public static void main(String[] args) {
+        String radio;
+        Double area;
+        Scanner num = new Scanner(System.in);
+        try {
+            System.out.print("Introduce el radio de un Círculo: ");
+            radio = num.next();
+            double doble = Double.parseDouble(radio);
+            area = Math.pow(doble,2)*PI;
+            System.out.println("El área del Círculo es: " + area);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+    }
+
