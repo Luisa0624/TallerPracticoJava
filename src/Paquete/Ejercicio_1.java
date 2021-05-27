ff88ceb524b44bc1253108f5a0d48987624fff7e
Luisa Gil <luisamariagilgarcia24@gmail.com>
2021-05-27 12:15:04 -0500

    Se realiza Ejercicio 1

---
+++ src/Paquete/Ejercicio_1.java
@@ -0,0 +1,18 @@
+package Paquete;
+
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+        int numero1 = 35;
+        int numero2 = 85;
+
+        if(numero1>numero2){
+            System.out.println("El número 1 es mayor");
+        }else if(numero1<numero2){
+            System.out.println("El número 2 es mayor");
+        }else if(numero1==numero2){
+            System.out.println("Los números son iguales");
+        }
+    }
+
+}
